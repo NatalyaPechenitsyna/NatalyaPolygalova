4bf3d698b38f877ad3325db8cf70782a28f43785
natal <https://github.com/NatalyaPechenitsyna/NatalyaPolygalova_LevelUp.git>
2019-12-14 00:26:33 +0500

    Добавлен общий класс

---
+++ src/test/java/ru/levelup/Natalia/Polygalova/qa/homework_5/ru/levelup/Natalia/Polygalova/qa/homework_5_selenium/Selenium.java
@@ -0,0 +1,31 @@
+package ru.levelup.Natalia.Polygalova.qa.homework_5.ru.levelup.Natalia.Polygalova.qa.homework_5_selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class Selenium {
+
+    protected WebDriver driver;
+
+
+    @BeforeSuite
+    public void setUpSuite(){
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod
+    public void setUpTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+    }
+    @AfterMethod
+    public void tearDownBrowser() {
+        driver.quit();
+    }
+}
