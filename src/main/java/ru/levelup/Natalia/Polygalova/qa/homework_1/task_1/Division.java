a47f1d3190ef2efd3563ae3c9977e8d1889027c6
natal <https://github.com/NatalyaPechenitsyna/NatalyaPolygalova_LevelUp.git>
2019-12-24 18:53:30 +0500

    Удалены файлы из HW1

--- src/main/java/ru/levelup/Natalia/Polygalova/qa/homework_1/task_1/Division.java
+++
@@ -1,7 +0,0 @@
-package ru.levelup.Natalia.Polygalova.qa.homework_1.task_1;
-
-public class Division {
-    public double divis(double num1, double num2) {
-        return (num1 / num2);
-    }
-}
--- src/main/java/ru/levelup/Natalia/Polygalova/qa/homework_1/task_1/Subtraction.java
+++
@@ -1,7 +0,0 @@
-package ru.levelup.Natalia.Polygalova.qa.homework_1.task_1;
-
-public class Subtraction {
-    public double subtr(double num1, double num2) {
-        return (num1 - num2);
-    }
-}
